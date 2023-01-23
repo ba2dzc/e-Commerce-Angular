088c4210d9bb9a49c7be80e1a5205cb22f4d1ed4
ba2dzc <batu.duzcu@gmail.com>
2023-01-23 21:34:29 +0300

    filter component

---
+++ src/app/components/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, OnInit,Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html'
+})
+export class FiltersComponent implements OnInit {
+  @Output() showCategory=new EventEmitter<string>()
+  categories=['shoes','gloves'];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onShowCategory(category:string):void{
+    this.showCategory.emit(category);
+  }
+}
