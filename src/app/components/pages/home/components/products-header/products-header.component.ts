8c4e9914679a83f57fd4efaa1ce5a7e6993d3f17
ba2dzc <batu.duzcu@gmail.com>
2023-01-23 21:37:22 +0300

    add listing items

--- src/app/components/pages/home/components/products-header/products-header.component.ts
+++ src/app/components/pages/home/components/products-header/products-header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
-  templateUrl: './products-header.component.html',
-  styleUrls: ['./products-header.component.css']
+  templateUrl: './products-header.component.html'
 })
 export class ProductsHeaderComponent implements OnInit {
-
+  @Output() columnsCountChange=new EventEmitter<number>();
+  sort="desc"
+  itemShowCount=12;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSortUpdate(newSort:string):void{
+    this.sort=newSort;
+  }
+
+  onItemsUpdate(count:number):void{
+    this.itemShowCount=count;
+  }
+
+  onColumnsUpdate(colsNum:number):void{
+    this.columnsCountChange.emit(colsNum)
+  }
 }
