37dd8b63b46502f116369ec770c4f03f59f55735
ba2dzc <batu.duzcu@gmail.com>
2023-01-27 15:31:22 +0300

    get categories from API

--- src/app/components/pages/home/components/filters/filters.component.ts
+++ src/app/components/pages/home/components/filters/filters.component.ts
@@ -1,15 +1,29 @@
-import { Component, EventEmitter, OnInit,Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit,Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html'
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent implements OnInit, OnDestroy {
   @Output() showCategory=new EventEmitter<string>()
-  categories=['shoes','gloves'];
-  constructor() { }
+
+  categoriesSubscription:Subscription|undefined
+  categories:Array<string>|undefined;
+  constructor(private storeService:StoreService) { }
 
   ngOnInit() {
+    this.categoriesSubscription=this.storeService.getAllCategories()
+    .subscribe((response)=>{
+      this.categories=response
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.categoriesSubscription){
+      this.categoriesSubscription.unsubscribe()
+    }
   }
 
   onShowCategory(category:string):void{
