452b49e21bb05820fe675221bbf12a12ba5fe275
ba2dzc <batu.duzcu@gmail.com>
2023-01-28 12:30:27 +0300

    checkout function added

--- src/app/components/pages/cart/cart.component.ts
+++ src/app/components/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -24,7 +26,7 @@ export class CartComponent implements OnInit {
     'total',
     'action'
   ]
-  constructor(private cartService:CartService) { }
+  constructor(private cartService:CartService,private http:HttpClient) { }
 
   ngOnInit() {
     this.dataSource=this.cart.items;
@@ -51,5 +53,16 @@ export class CartComponent implements OnInit {
 
   onRemoveQuantity(item:CartItem):void{
     this.cartService.removeQuantity(item)
+  }
+
+  onCheckout():void{
+    this.http.post('http://localhost:4242/checkout',{
+      items: this.cart.items
+    }).subscribe(async(res:any)=>{
+      let stripe=await loadStripe('##Key##');
+      stripe?.redirectToCheckout({
+        sessionId:res.id
+      })
+    })
   }
 }
